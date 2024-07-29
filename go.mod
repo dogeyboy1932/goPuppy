643f042606cc88f96755e5c7fa16d2edcc9d2fc3
dogeyboy1932 <vg23@illinois.edu>
2024-07-29 14:51:18 -0500

    Added dog changes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dogeyboy1932/goPuppy
 
 go 1.22.5
+
+require github.com/dogeyboy1932/goDog v0.0.0-20240729195011-dd68957cf612 // indirect
--- goPuppy.go
+++ goPuppy.go
@@ -1,9 +1,17 @@
 package goPuppy
 
+import (
+	"github.com/dogeyboy1932/goDog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return goDog.WhenGrownUp("Woof!")
 }
